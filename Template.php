41732609c93e127d63c412170a090a20152ade25
William Young <35644654+WilliamYoung92@users.noreply.github.com>
2019-05-15 23:14:25 -0400

    Add files via upload
    This template is used to update the menu bar, and footer while allowing for an editable region. This is to minimize effort to update the project at any given time.

---
+++ Template.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Untitled Document</title>
+<!-- TemplateEndEditable -->
+<style type="text/css">
+.left {
+	text-align: left;
+	color: #000;
+	vertical-align: top;
+}
+.header {
+	background-color: #002857;
+	height: 100px;
+	width: 1000px;
+}
+.page {
+	width: 1000px;
+	color: #FFFFFF;
+	background-color: #002857;
+	background-image: none;
+	background-repeat: no-repeat;
+}
+.body {
+}
+.header2 {
+	height: 100px;
+	width: 1000px;
+	vertical-align: text-bottom;
+	font-size: 16px;
+}
+.user {
+	float: right;
+	vertical-align: baseline;
+}
+.title {
+	vertical-align: middle;
+}
+.logo {
+	float: left;
+	height: 144px;
+	width: 144px;
+	text-align: left;
+	vertical-align: top;
+	background-image: url(file:///C|/Users/Wil/Desktop/logo.png);
+	background-repeat: no-repeat;
+}
+</style>
+<link href="file:///C|/Users/Wil/Desktop/SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+<script src="file:///C|/Users/Wil/Desktop/SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body class="left">
+<span class="MenuBarHorizontal"></span>
+<div class="header2">
+  <div class="user">Admin Profile / Sign Out </div>
+  <div class="title">
+  <div>
+    <div class="logo"></div>
+  </div>
+  </div>
+  <p>FAU eLectronics admin</p>
+  <br />
+  <ul id="MenuBar2" class="MenuBarHorizontal">
+    <li class="MenuBarHorizontal"><a class="MenuBarItemSubmenu" href="#">Inventory Management</a>
+      <ul>
+        <li><a href="#">Edit Inventory</a></li>
+        <li><a href="#">Create Kit</a></li>
+        <li><a href="#">Customer Item Requests</a></li>
+        <li><a href="#">Item Information Update Request</a></li>
+        <li><a href="#">Order Items from Vendor</a></li>
+      </ul>
+    </li>
+    <li class="MenuBarHorizontal"><span class="MenuBarItemIE"><a href="#">Staff Accounts</a></span></li>
+    <li class="MenuBarHorizontal"><a href="#" class="MenuBarHorizontal">Client Accounts</a></li>
+    <li class="MenuBarHorizontal"><a href="#" class="MenuBarItemSubmenu">Reports</a>
+      <ul>
+        <li><a href="#">Inventory Repoorts</a></li>
+        <li><a href="#">Vendor Reports</a></li>
+        <li><a href="#">Client Reports</a></li>
+        <li><a href="#">Staff Reports</a></li>
+      </ul>
+    </li>
+  </ul>
+  <p><br />
+    <br />
+  </p>
+</div>
+<div class="page">Admin &gt; Home</div>
+<p>&nbsp;</p>
+<!-- TemplateBeginEditable name="EditRegion1" -->
+<div class="body">The admin can do all things, the only limit is yourself.</div>
+<!-- TemplateEndEditable -->
+<p>&nbsp; </p>
+<p>&nbsp;</p>
+<p>&nbsp; </p>
+<script type="text/javascript">
+var MenuBar2 = new Spry.Widget.MenuBar("MenuBar2", {imgDown:"SpryAssets/SpryMenuBarDownHover.gif", imgRight:"SpryAssets/SpryMenuBarRightHover.gif"});
+</script>
+</body>
+</html>
